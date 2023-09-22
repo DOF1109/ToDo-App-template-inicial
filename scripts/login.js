4ba94a95c9760a92fa94da47aa6f39df20ddf3fd
DOF1109 <friasdaniel.1109@gmail.com>
2023-09-21 22:55:59 -0300

    Completando las validaciones aún

--- scripts/login.js
+++ scripts/login.js
@@ -1,23 +1,45 @@
 window.addEventListener('load', function () {
 
-    /* ---------------------- obtenemos variables globales ---------------------- */
+    /* --------------- Obtenemos variables globales --------------- */
     const form = document.forms[0];
-    const email = document.querySelector('#inputEmail');
-    const password = document.querySelector('#inputPassword');
+    const email = document.getElementById('inputEmail');
+    const errorEmail = document.getElementById('errorEmail');
+    const password = document.getElementById('inputPassword');
+    const errorPassword = document.getElementById('errorEmail');
 
     const url = 'https://todo-api.ctd.academy/v1';
 
-    /* -------------------------------------------------------------------------- */
-    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-    /* -------------------------------------------------------------------------- */
+    /* --------------- Objeto para almacenar los errores de inputs --------------- */
+    estadoErrores = {
+        emailOK: false,
+        passwordOK: false
+    }
+    
+    function mostrarErrores(){
+        (estadoErrores.emailOK)? errorEmail.classList.add('invisible') : errorEmail.classList.remove('invisible');
+        (estadoErrores.passwordOK)? errorEmail.classList.add('invisible') : errorEmail.classList.remove('invisible');
+    }
+    
+    /* --------------- Validamos los datos mientras se ingresan --------------- */
+    form.addEventListener('change', () => {
+        console.log(email.value);
+        estadoErrores.emailOK = validarEmail(email.value);
+        estadoErrores.passwordOK = validarContrasenia(password.value);
+
+        mostrarErrores();
+    })
+
+    /* --------------- Escuchamos el submit y preparamos el envío --------------- */
     form.addEventListener('submit', (event) => {
         event.preventDefault();
 
+        // Creamos el cuerpo de la request
         const payload = {
             email: email.value,
             password: password.value
         };
 
+        // Configuramos la request del Fetch
         const setting = {
             method:'POST',
             body: JSON.stringify(payload),
@@ -26,14 +48,16 @@ window.addEventListener('load', function () {
             }
         }
 
+        // Lanzamos la consulta de login a la API
         realizarLogin(setting);
+
+        // Limpio los campos del formulario
+        form.reset();
     });
 
-    /* -------------------------------------------------------------------------- */
-    /*                     FUNCIÓN 2: Realizar el login [POST]                    */
-    /* -------------------------------------------------------------------------- */
+    /* --------------- Realizar el login [POST] --------------- */
     function realizarLogin(settings) {
-        console.log('lanzando la consulta a API');
+        console.log('Lanzando la consulta a API');
 
         fetch(`${url}/users/login`, settings)
         .then(response => {
